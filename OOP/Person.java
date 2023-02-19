ca99e7c8ba8f81d1f6507584ebd282496d8e8388
dayananikol <111488292+dayananikol@users.noreply.github.com>
2023-02-20 01:25:19 +0800

    Create Person.java

---
+++ OOP/Person.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class Person { 
+	
+	String name;
+	String gender;
+    double weight;
+    int jowa;
+	
+ 
+	
+//    Constructor
+	
+	Person (String name, String gender, double weight, int jowa){
+		
+	this.name = name;
+	this.gender = gender;
+	this.weight = weight;
+	this.jowa = jowa;
+	
+	 }
+	
+}
